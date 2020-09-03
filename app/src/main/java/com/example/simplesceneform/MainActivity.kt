8ecfb4f699aea2b9bf87cf1471340bc1c0d5a8e5
Renshi Asada <max_min_ab@outlook.jp>
2020-09-04 07:49:19 +0900

    add sceneform tutorial project

--- app/src/main/java/com/example/simplesceneform/MainActivity.kt
+++ app/src/main/java/com/example/simplesceneform/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.simplesceneform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var andyRenderable: ModelRenderable
+    private lateinit var arFragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.ar_fragment) as ArFragment
+
+        ModelRenderable.builder()
+            .setSource(this, R.raw.andy)
+            .build()
+            .thenAccept({ renderable -> andyRenderable = renderable })
+            .exceptionally(
+                { throwable ->
+                    null
+                })
+
+        arFragment.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
+            if (andyRenderable == null) {
+                return@setOnTapArPlaneListener
+            }
+
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment.arSceneView.scene)
+
+            val andy = TransformableNode(arFragment.transformationSystem)
+            andy.setParent(anchorNode)
+            andy.renderable = andyRenderable
+            andy.select()
+        }
     }
 }
